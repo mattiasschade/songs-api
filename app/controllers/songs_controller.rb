208d3d68cd6bc4c639ce71b1112e83f5538e5db5
Mattias Schade <mattiasschade@gmail.com>
2023-03-31 13:02:39 -0500

    Songs index, show, create, update and destroy

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,2 +1,41 @@
 class SongsController < ApplicationController
+
+  def index
+    @songs = Song.all
+    render :index
+  end
+
+  def show
+    @song = Song.find_by(id: params["id"])
+    render :show
+  end
+
+  def create
+    @song = Song.new(
+      title: params[:title], 
+      album: params[:album], 
+      artist: params[:description], 
+      year: params[:year])
+    
+    @song.save
+    render :show
+  end
+
+  def update
+    @song = Song.find_by(id: params["id"])
+    @song.title = params[:title] || @song.title
+    @song.album = params[:album] || @song.album
+    @song.artist = params[:artist] || @song.artist
+    @song.year = params[:year] || @song.year
+
+    @song.save
+    render :show
+  end
+
+  def destroy
+    @song = Song.find_by(id: params["id"])
+    @song.destroy
+    render json: {message: "The song has been successfully deleted"}
+  end
+
 end
--- app/views/songs/_song.json.jbuilder
+++ app/views/songs/_song.json.jbuilder
@@ -1,2 +1,7 @@
-json.extract! song, :id, :created_at, :updated_at
-json.url song_url(song, format: :json)
+json.id song.id
+json.title song.title
+json.album song.album
+json.artist song.artist
+json.year song.year
+json.created_at song.created_at
+json.updated_at song.updated_at
